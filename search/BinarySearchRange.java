1a37bb2408c11618b1414ee63cb5fbef467bde03
Ztrix <zl700@outlook.com>
2022-07-18 17:37:30 +0800

    Binary search range

---
+++ search/BinarySearchRange.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BinarySearchRange<T extends Comparable<T>> {
+    public BinarySearchRange() {
+
+    }
+
+    public int[] searchRange(T[] arr, T target) {
+        int l = 0;
+        int r = arr.length - 1;
+        while (r - l > 1) {
+            int m = (l + r) / 2;
+            int res = target.compareTo(arr[m]);
+            if (res == 0) {
+                return new int[] { m, m };
+            } else if (res > 0) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        int compareArr0AndTarget = arr[0].compareTo(target);
+        int compareArrLastAndTarget = arr[r].compareTo(target);
+        if (l == 0 && compareArr0AndTarget > 0) {
+            return new int[] { -1, 0 };
+        } else if (r == arr.length - 1 && compareArrLastAndTarget < 0) {
+            return new int[] { r, arr.length };
+        } else if (compareArr0AndTarget == 0) {
+            return new int[] { l, l };
+        } else if (compareArrLastAndTarget == 0) {
+            return new int[] { r, r };
+        }
+        return new int[] { l, r };
+    }
+
+    public int[] searchRange(List<T> list, T target) {
+        int l = 0;
+        int r = list.size() - 1;
+        while (r - l > 1) {
+            int m = (l + r) / 2;
+            int res = target.compareTo(list.get(m));
+            if (res == 0) {
+                return new int[] { m, m };
+            } else if (res > 0) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        int compareArr0AndTarget = list.get(0).compareTo(target);
+        int compareArrLastAndTarget = list.get(list.size() - 1).compareTo(target);
+        if (l == 0 && compareArr0AndTarget > 0) {
+            return new int[] { -1, 0 };
+        } else if (r == list.size() - 1 && compareArrLastAndTarget < 0) {
+            return new int[] { r, list.size() - 1 };
+        } else if (compareArr0AndTarget == 0) {
+            return new int[] { l, l };
+        } else if (compareArrLastAndTarget == 0) {
+            return new int[] { r, r };
+        }
+        return new int[] { l, r };
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[] { 1, 2, 3, 4, 7, 8, 9, 11, 14, 19 };
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 7, 8, 9, 11, 14, 19));
+        BinarySearchRange<Integer> bs = new BinarySearchRange<>();
+        int[] range1 = bs.searchRange(arr, 20);
+        int[] range2 = bs.searchRange(arr, 20);
+        for (int i : range1) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+        for (int i : range2) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
